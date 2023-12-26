7b219818d0f1e30353a3181cfdcbc5ba9a421b9c
OnlyPascal <chuyengianhorang@gmail.com>
2023-12-26 12:13:36 +0700

    add test screen

---
+++ app/src/main/java/com/example/teenduh/view/activity/TestFeature.java
@@ -0,0 +1,16 @@
+package com.example.teenduh.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.teenduh.R;
+
+public class TestFeature extends AppCompatActivity {
+  
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_test_feature);
+  }
+}
---
+++ app/src/main/java/com/example/teenduh/view/activity/TestSuccess.java
@@ -0,0 +1,16 @@
+ package com.example.teenduh.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.teenduh.R;
+
+public class TestSuccess extends AppCompatActivity {
+  
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_test_success);
+  }
+}
