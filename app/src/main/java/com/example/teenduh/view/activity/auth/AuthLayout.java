ce74436b309d59b24c271393e66015d79d497326
Pavel Potemkin <specialforbtsu@gmail.com>
2024-01-04 16:14:49 +0700

    facebook login test

--- app/src/main/java/com/example/teenduh/view/activity/auth/AuthLayout.java
+++ app/src/main/java/com/example/teenduh/view/activity/auth/AuthLayout.java
@@ -1,9 +1,14 @@
 package com.example.teenduh.view.activity.auth;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -13,6 +18,12 @@ import com.example.teenduh.R;
 import com.example.teenduh.util.AndroidUtil;
 import com.example.teenduh.util.FirebaseUtil;
 import com.example.teenduh.view.activity.TestSuccess;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.identity.BeginSignInRequest;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -23,14 +34,26 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
 public class AuthLayout extends AppCompatActivity {
   private final String TAG = "AuthLayout";
+  private static final String EMAIL = "email";
   private final int PHONE_AUTH_REQUEST_CODE = 1;
   private final int GOOGLE_AUTH_REQUEST_CODE = 2;
+  private final int FACEBOOK_AUTH_REQUEST_CODE = 3;
+  private CallbackManager callbackManager;
+  private Button googleLoginButton;
+  private Button loginButton;
+  private LoginButton facebookLoginButton;
+  private Button phoneNumberLoginButton;
   private GoogleSignInOptions googleSignInOptions;
   private GoogleSignInClient googleSignInClient;
 
@@ -48,12 +71,56 @@ public class AuthLayout extends AppCompatActivity {
 
     googleSignInClient = GoogleSignIn.getClient(AuthLayout.this, googleSignInOptions);
 
+    loginButton = findViewById(R.id.btn_facebook_log_in);
+    facebookLoginButton = findViewById(R.id.login_button);
+
+    callbackManager = CallbackManager.Factory.create();
+
+    facebookLoginButton.setReadPermissions(Arrays.asList(EMAIL));
+
+    facebookLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+      @Override
+      public void onSuccess(LoginResult loginResult) {
+        Log.d(TAG, "facebook:onSuccess:" + loginResult);
+        handleFacebookAccessToken(loginResult.getAccessToken());
+      }
+
+      @Override
+      public void onCancel() {
+        Log.d(TAG, "facebook:onCancel");
+      }
+
+      @Override
+      public void onError(FacebookException exception) {
+        Log.d(TAG, "facebook:onError", exception);
+      }
+    });
+
     FirebaseUser firebaseUser = FirebaseUtil.getUser();
-    if (firebaseUser != null) {
+    // TODO: uncomment in production
+    /*if (firebaseUser != null) {
       AndroidUtil._startActivity(AuthLayout.this, TestSuccess.class);
       finish();
-    }
+    }*/
   }
+
+  private void handleFacebookAccessToken(AccessToken token) {
+    Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+    AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+    FirebaseUtil.getFirebaseAuth().signInWithCredential(credential)
+      .addOnCompleteListener(this, task -> {
+        if (task.isSuccessful()) {
+          Log.d(TAG, "signInWithCredential:success");
+          FirebaseUser user = FirebaseUtil.getUser();
+        } else {
+          Log.w(TAG, "signInWithCredential:failure", task.getException());
+          Toast.makeText(AuthLayout.this, "Authentication failed.",
+            Toast.LENGTH_SHORT).show();
+        }
+      });
+  }
+
 
   @Override
   protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -91,6 +158,8 @@ public class AuthLayout extends AppCompatActivity {
               }
             });
         break;
+      default:
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
   }
