20fc1ad65ad5a7b08d3b66d27fba422092958459
huan27042003 <huan27042003@gmail.com>
2024-01-18 15:49:33 +0700

    Add Subscription Item

--- app/src/main/java/com/example/teenduh/view/fragment/Profile.java
+++ app/src/main/java/com/example/teenduh/view/fragment/Profile.java
@@ -2,7 +2,6 @@ package com.example.teenduh.view.fragment;
 
 import android.app.Activity;
 import android.app.Dialog;
-import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -28,7 +27,7 @@ import androidx.viewpager2.widget.ViewPager2;
 import com.example.teenduh.R;
 import com.example.teenduh.model.Image;
 import com.example.teenduh._util.AndroidUtil;
-import com.example.teenduh.view.adapter.ImageAdapter;
+import com.example.teenduh.view.adapter.SubscriptionAdapter;
 import com.google.android.material.progressindicator.CircularProgressIndicator;
 
 import java.util.ArrayList;
@@ -41,6 +40,7 @@ public class Profile extends Fragment {
   boolean isProceedPayment;
   boolean isDeposit;
   ViewPager2 viewPager2;
+  private CardView circle1,circle2;
   List<Image> imageList;
   private CircularProgressIndicator progressIndicator;
   private Handler sliderHandler;
@@ -64,6 +64,9 @@ public class Profile extends Fragment {
     buttonSuperLike = view.findViewById(R.id.button10);
     buttonSuperFind = view.findViewById(R.id.button11);
     progressIndicator = view.findViewById(R.id.progressBar);
+    viewPager2 = view.findViewById(R.id.viewPager2);
+    circle1 = view.findViewById(R.id.circle1);
+    circle2 = view.findViewById(R.id.circle2);
     
     buttonSuperLike.setOnClickListener(v -> {
       Toast.makeText(activity, "Super Like", Toast.LENGTH_SHORT).show();
@@ -74,13 +77,30 @@ public class Profile extends Fragment {
       Toast.makeText(activity, "Super Find", Toast.LENGTH_SHORT).show();
       showSuperLikeDialog();
     });
-    
-    viewPager2 = view.findViewById(R.id.viewPager2);
+
+    List<View> viewList = new ArrayList<>();
+    View view1 = LayoutInflater.from(getContext()).inflate(R.layout.item_card, container, false);
+    View view2 = LayoutInflater.from(getContext()).inflate(R.layout.item_tinder_subsription, container, false);
+    viewList.add(view1);
+    viewList.add(view2);
+
     viewPager2.bringToFront();
-    imageList = new ArrayList<Image>();
-    imageList.add(new Image(R.drawable.tinder_plus, "nuill"));
-    imageList.add(new Image(R.drawable.tinder_gold, "nuill"));
-    viewPager2.setAdapter(new ImageAdapter(imageList, viewPager2));
+    viewPager2.setAdapter(new SubscriptionAdapter(viewPager2));
+
+    viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+      @Override
+      public void onPageSelected(int position) {
+        super.onPageSelected(position);
+        if (position == 0) {
+          circle1.setForeground(getResources().getDrawable(R.drawable.btn_layout));
+          circle2.setForeground(getResources().getDrawable(R.drawable.btn_unselected));
+        } else if (position == 1) {
+          circle2.setForeground(getResources().getDrawable(R.drawable.btn_layout));
+          circle1.setForeground(getResources().getDrawable(R.drawable.btn_unselected));
+        }
+      }
+    });
+
     viewPager2.setOffscreenPageLimit(3);
     viewPager2.setClipChildren(false);
     viewPager2.setClipToPadding(false);
