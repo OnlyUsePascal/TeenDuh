f970ecd45b1f10568496a5e2712a49f715b192bd
Potemkin Pavel <specialforbtsu@gmail.com>
2024-11-16 17:15:05 +0700

    fix gradle errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,54 +36,54 @@ android {
 }
 
 dependencies {
-  implementation("com.google.android.gms:play-services-wallet:19.3.0-beta01")
-  implementation("androidx.appcompat:appcompat:1.6.1")
-  implementation("com.google.android.material:material:1.11.0")
-  implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-  implementation("androidx.navigation:navigation-fragment:2.7.5")
-  implementation("androidx.navigation:navigation-ui:2.7.5")
-  implementation("androidx.annotation:annotation:1.8.0")
-  implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
-  implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+  implementation("com.google.android.gms:play-services-wallet:19.4.0")
+  implementation("androidx.appcompat:appcompat:1.7.0")
+  implementation("com.google.android.material:material:1.12.0")
+  implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+  implementation("androidx.navigation:navigation-fragment:2.8.4")
+  implementation("androidx.navigation:navigation-ui:2.8.4")
+  implementation("androidx.annotation:annotation:1.9.1")
+  implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
+  implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
   implementation("pl.bclogic:pulsator4droid:1.0.3")
   implementation("de.hdodenhof:circleimageview:3.1.0")
 
   // auth
-  implementation("com.google.android.gms:play-services-auth:20.7.0")
+  implementation("com.google.android.gms:play-services-auth:21.2.0")
 //  implementation("com.facebook.android:facebook-login:latest.release")
-  implementation("com.squareup.okhttp3:okhttp:4.10.0")
+  implementation("com.squareup.okhttp3:okhttp:4.12.0")
 
   // testing
-  androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+  androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
   testImplementation("junit:junit:4.13.2")
-  androidTestImplementation("androidx.test.ext:junit:1.1.5")
+  androidTestImplementation("androidx.test.ext:junit:1.2.1")
 
   // packaging
   implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
   // loading animation
-  coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
+  coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.3")
   implementation("com.airbnb.android:lottie:6.2.0")
 
   //map
-  implementation("com.google.android.gms:play-services-maps:18.2.0")
-  implementation("com.google.android.gms:play-services-location:21.0.1")
+  implementation("com.google.android.gms:play-services-maps:19.0.0")
+  implementation("com.google.android.gms:play-services-location:21.3.0")
   implementation("com.google.maps:google-maps-services:2.2.0")
-  implementation("org.slf4j:slf4j-simple:2.0.9");
-  implementation("com.google.maps.android:android-maps-utils:3.8.0");
-  implementation("com.google.android.libraries.places:places:2.6.0")
+  implementation("org.slf4j:slf4j-simple:2.0.9")
+  implementation("com.google.maps.android:android-maps-utils:3.8.0")
+  implementation("com.google.android.libraries.places:places:4.1.0")
 
   //firebase
-  implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
-  implementation("com.google.firebase:firebase-auth:22.3.0")
-  implementation("com.google.firebase:firebase-firestore:24.10.0")
-  implementation("com.google.firebase:firebase-analytics:21.5.0")
+  implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
+  implementation("com.google.firebase:firebase-auth:23.1.0")
+  implementation("com.google.firebase:firebase-firestore:25.1.1")
+  implementation("com.google.firebase:firebase-analytics:22.1.2")
   implementation("com.firebaseui:firebase-ui-firestore:8.0.2")
-  implementation("com.google.firebase:firebase-firestore-ktx:24.10.0")
-  implementation("com.google.firebase:firebase-storage-ktx:20.3.0")
-  implementation("com.google.firebase:firebase-messaging-ktx:23.4.0")
-  implementation("com.google.firebase:firebase-messaging:23.4.0")
-  implementation("com.google.firebase:firebase-storage:20.3.0")
+  implementation("com.google.firebase:firebase-firestore-ktx:25.1.1")
+  implementation("com.google.firebase:firebase-storage-ktx:21.0.1")
+  implementation("com.google.firebase:firebase-messaging-ktx:24.1.0")
+  implementation("com.google.firebase:firebase-messaging:24.1.0")
+  implementation("com.google.firebase:firebase-storage:21.0.1")
 
   // international phone number hadling
   implementation("com.fredporciuncula:phonemoji:1.5.2")
@@ -103,4 +103,9 @@ dependencies {
   implementation("org.jfrog.cardinalcommerce.gradle:cardinalmobilesdk:2.2.7-5")
   implementation("com.stripe:stripe-android:20.36.1")
 
+}
+java {
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(17)
+  }
 }
